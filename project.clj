93cf47c0b73faf3e2b86a436cdb2e684a36d32c4
Luke VanderHart <luke@vanderhart.net>
2013-06-19 16:03:50 -0400

    Add :min-lein-version for Heroku goodness

--- project.clj
+++ project.clj
@@ -3,6 +3,7 @@
   post-receive hook HTTP requests from GitHub."
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :min-lein-version "2.0.0"
   :plugins [[lein-ring "0.8.5"]]
   :dependencies [[org.clojure/clojure "1.5.1"]
                  [org.clojure/data.json "0.2.2"]
